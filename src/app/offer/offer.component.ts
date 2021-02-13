018b7e84f0a32e132a079fe45e26041973da6e5b
jakub_ant <jakub.antosiewicz.1998@gmail.com>
2021-02-13 08:55:15 +0100

    Add basic error handling and unsubscribe.

--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -1,10 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { DbServiceService } from '../shared/services/db-service.service';
+import {
+  Component,
+  OnDestroy,
+  OnInit
+} from '@angular/core';
+import {
+  DbServiceService
+} from '../shared/services/db-service.service';
 
 interface Item {
   "id": string,
   "name": string,
-  "desc": {"gender": string, "size": number},
+  "desc": {
+    "gender": string,
+    "size": number
+  },
   "price": number,
   "availableItems": number,
   "imageURL": string
@@ -14,25 +23,39 @@ interface Item {
   templateUrl: './offer.component.html',
   styleUrls: ['./offer.component.scss']
 })
-export class OfferComponent implements OnInit {
+export class OfferComponent implements OnInit, OnDestroy {
   items: any
-  itemsArray: Item [] = [];
+  itemsArray: Item[] = [];
+  offerSubscription: any;
+  
 
-  constructor(private dbService:DbServiceService) { }
+  constructor(private dbService: DbServiceService) {
+  }
+
+  renderOffers() {
+    for (const key in this.items) {
+      if (Object.prototype.hasOwnProperty.call(this.items, key)) {
+        const element = this.items[key];
+        element.id = key;
+        this.itemsArray.push(element);
+        console.log(this.itemsArray)
+      }
+    }
+  }
 
   ngOnInit(): void {
-    this.dbService.getItems()
-    .subscribe(items=>{this.items = items;
-      for (const key in this.items) {
-        if (Object.prototype.hasOwnProperty.call(this.items, key)) {
-          const element = this.items[key];
-          element.id = key;
-          this.itemsArray.push(element);
-          console.log(this.itemsArray)
-        }
-      }
-    })
+    this.offerSubscription = this.dbService.getItems()
+      .subscribe(items => {
+        this.items = items
+        this.renderOffers()
+      },
+      err=>console.log(err)
+      )
 
+  }
+
+  ngOnDestroy(){
+    this.offerSubscription.unsubscribe()
   }
 
 }
