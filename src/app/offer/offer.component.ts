3bb1bcaacba6f5a7f6ffd9c82b9f9b8e89256612
jakub_ant <jakub.antosiewicz.1998@gmail.com>
2021-03-22 17:50:31 +0100

    Remove unused imports

--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -10,9 +10,6 @@ import {
   AuthServiceService
 } from '../shared/services/auth-service.service';
 import {
-  DbServiceService
-} from '../shared/services/db-service.service';
-import {
   User
 } from '../shared/user.model';
 import {
@@ -20,19 +17,8 @@ import {
 } from './offer.service';
 import{ErrorMsg} from '../shared/error-msg.model';
 import { OfferItem } from '../shared/offer-item.model';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
-// interface Item {
-//   "id": string,
-//   "name": string,
-//   "desc": {
-//     "gender": string,
-//     "size": number
-//   },
-//   "price": number,
-//   "availableItems": number,
-//   "imageURL": string
-// }
+
 @Component({
   selector: 'app-offer',
   templateUrl: './offer.component.html',
@@ -50,9 +36,7 @@ export class OfferComponent implements OnInit, OnDestroy {
     errorMsg: 'Wystąpił błąd'
   }
 
-
-
-  constructor(private dbService: DbServiceService, private authService: AuthServiceService, private offerService: OfferService) {}
+  constructor(private authService: AuthServiceService, private offerService: OfferService) {}
   toggleError(event:boolean){
     if(event){
       this.showError()
@@ -100,22 +84,5 @@ export class OfferComponent implements OnInit, OnDestroy {
     if (this.authSubscription) this.authSubscription.unsubscribe()
   }
 
-  // addToCart(event: any) {
-  //   const productId: string = event.target.dataset.id;
-  //   console.log(productId)
-  //   console.log(this.loggedInUser)
-  //   if (!this.loggedInUser) return;
-  //   this.loggedInUser.shoppingCart.length = 0;
-  //   this.loggedInUser.shoppingCart.push(productId)
-  //   this.dbService.addToCart(this.loggedInUser).subscribe(
-  //     () => {
-  //       this.hideError()
-  //       if (this.loggedInUser) {
-  //         this.dbService.getCurrentCart(this.loggedInUser.localId).subscribe()
-  //       }
-  //     },
-  //     (err) => this.showError()
-  //   )
-  // }
 
 }
